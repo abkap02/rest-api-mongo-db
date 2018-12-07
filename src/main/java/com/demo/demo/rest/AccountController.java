f17b845dbf5b780137dbe3e37cd8a004d323d36a
abkap02 <kapoor003@knights.gannon.edu>
2018-12-06 23:36:16 -0500

    Initial commit

---
+++ src/main/java/com/demo/demo/rest/AccountController.java
@@ -0,0 +1,53 @@
+package com.demo.demo.rest;
+
+import com.demo.demo.domain.response.AccountResponse;
+import com.demo.demo.domain.response.NotFound;
+import com.demo.demo.model.Account;
+import com.demo.demo.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+public class AccountController {
+
+    @Autowired
+    AccountService accountService;
+
+    @RequestMapping(value = "/account", method = RequestMethod.GET)
+    public ResponseEntity getAccount(@RequestParam  String id) {
+        Account account = accountService.getAccount(id);
+        if (account != null) {
+            return ResponseEntity.ok().body(account);
+        }
+        NotFound notFound = new NotFound();
+        notFound.setMessage("Account not found for id " + id );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(notFound);
+    }
+
+    @RequestMapping(value = "/accounts", method = RequestMethod.GET)
+    public ResponseEntity getAccounts() {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(accountService.getAll());
+    }
+
+    @RequestMapping(value = "/account", method = RequestMethod.POST)
+    public ResponseEntity<AccountResponse> createAccount(@RequestBody Account account) {
+
+        Account createdAccount = accountService.createAccount(account);
+        AccountResponse accountResponse = new AccountResponse();
+        accountResponse.setId(createdAccount.getId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(accountResponse);
+    }
+
+    @RequestMapping(value = "/account", method = RequestMethod.DELETE)
+    public ResponseEntity deleteAccount(@RequestParam String id) {
+        return ResponseEntity.status(HttpStatus.OK).body(accountService.delete(id));
+    }
+
+    @RequestMapping(value = "/account", method = RequestMethod.PUT)
+    public ResponseEntity updateAccount(@RequestBody Account account) {
+        return ResponseEntity.status(HttpStatus.OK).body(accountService.updateAccount(account));
+    }
+}
---
+++ src/main/java/com/demo/demo/service/AccountService.java
@@ -0,0 +1,44 @@
+package com.demo.demo.service;
+
+import com.demo.demo.model.Account;
+import com.demo.demo.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AccountService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public Account getAccount(String id) {
+        return accountRepository.findById(id).orElse(null);
+    }
+
+    public List<Account> getAll() {
+        return accountRepository.findAll();
+    }
+
+    public Account createAccount(Account account) {
+        return accountRepository.save(account);
+    }
+
+    public Boolean delete(String id) {
+        Boolean exists = accountRepository.existsById(id);
+        if (exists == true) {
+            accountRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Account updateAccount(Account account){
+        return accountRepository.save(account);
+    }
+
+}
+
+
